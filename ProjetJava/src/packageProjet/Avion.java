ad97cffac52e0a4b4be49e873057062bd3c0c943
formation <formation@ubuntu.ubuntu-domain>
2019-04-03 16:36:05 +0200

    deuxieme commit

---
+++ ProjetJava/src/packageProjet/Avion.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package packageProjet;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @author formation
+ *
+ */
+public class Avion {
+	
+	private String matricule;
+	private String modèle;
+	private Aeroport localisation;
+	private int capacite;
+	private double rayondaction;
+	private double prixkm;
+	private List<Boolean> listplace;
+	
+	
+	/**
+	 * @param date
+	 * @return
+	 */
+	public boolean enVol(String date) {
+		//TODOimplementer la methode
+		
+	}
+	
+	public String getMatricule() {
+		return matricule;
+	}
+	
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	
+	public String getModèle() {
+		return modèle;
+	}
+	
+	public void setModèle(String modèle) {
+		this.modèle = modèle;
+	}
+	
+	public Aeroport getLocalisation() {
+		return localisation;
+	}
+	
+	public void setLocalisation(Aeroport localisation) {
+		this.localisation = localisation;
+	}
+	
+	public int getCapacite() {
+		return capacite;
+	}
+	
+	public void setCapacite(int capacite) {
+		this.capacite = capacite;
+	}
+	
+	public double getRayondaction() {
+		return rayondaction;
+	}
+	
+	public void setRayondaction(double rayondaction) {
+		this.rayondaction = rayondaction;
+	}
+	
+	public double getPrixkm() {
+		return prixkm;
+	}
+	
+	public void setPrixkm(double prixkm) {
+		this.prixkm = prixkm;
+	}
+	
+	public List<Boolean> getListplace() {
+		return listplace;
+	}
+	
+	public void setListplace(List<Boolean> listplace) {
+		this.listplace = listplace;
+	}
+}
