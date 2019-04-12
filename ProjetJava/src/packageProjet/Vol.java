724a5c2a95428bdeebb01a57b16068dffc97736d
formation <formation@ubuntu.ubuntu-domain>
2019-04-12 12:11:18 +0200

    implementation create vol

--- ProjetJava/src/packageProjet/Vol.java
+++ ProjetJava/src/packageProjet/Vol.java
@@ -14,7 +14,12 @@ public class Vol {
  	
  	
  	
- 	
+ 	public Vol(Aeroport D, Aeroport A, int date) {
+ 		this.aeroportD = D;
+ 		this.aeroportA = A;
+ 		this.date = date;
+ 		this.listVoyageur = new ArrayList<Voyageur>();
+ 	}
  	
  	
 	/**
@@ -35,24 +40,14 @@ public class Vol {
 	public Avion getAvion() {
 		return avion;
 	}
-	/**
-	 * @param avion the avion to set
-	 */
-	public void setAvion(Avion avion) {
-		this.avion = avion;
-	}
+
 	/**
 	 * @return the equipage
 	 */
 	public Equipage getEquipage() {
 		return equipage;
 	}
-	/**
-	 * @param equipage the equipage to set
-	 */
-	public void setEquipage(Equipage equipage) {
-		this.equipage = equipage;
-	}
+	
 	/**
 	 * @return the aeroportD
 	 */
@@ -83,12 +78,7 @@ public class Vol {
 	public ArrayList<Voyageur> getListVoyageur() {
 		return listVoyageur;
 	}
-	/**
-	 * @param listVoyageur the listVoyageur to set
-	 */
-	public void setListVoyageur(ArrayList<Voyageur> listVoyageur) {
-		this.listVoyageur = listVoyageur;
-	}
+
 	/**
 	 * @return the date
 	 */
@@ -102,18 +92,74 @@ public class Vol {
 		this.date = date;
 	} 
  	
-
+	/**
+	 * @param v le voyageur que l'on ajoute a la liste des passagers du vol
+	 */
 	public void addVoyageur(Voyageur v) {
 		this.listVoyageur.add(v);
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public double calculDistance() {
 		return Math.sqrt(Math.pow(this.aeroportA.getCoordonneeX() - this.aeroportD.getCoordonneeX(), 2) + Math.pow(this.aeroportA.getCoordonneeY() - this.aeroportD.getCoordonneeY(), 2) );
 	}
 	
 	
+	/**
+	 * @param avion l'avion que l'on affrète au vol
+	 */
+	public void addAvion(Avion avion) {
+		this.avion = avion;
+	}
+	
+	/**
+	 * @param equipage l'équipage que l'on affrète au vol 
+	 */
+	public void addEquipage(Equipage equipage) {
+		this.equipage = equipage;
+	}
  	
- 	
- 	
+	public void createVol(Aeroport D, Aeroport A, int date, Compagnie c) {
+		Vol v = new Vol(D, A, date);
+		double dist = v.calculDistance();
+		if(dist > 2000) {
+			for(Avion a : D.getListAvion()) {
+				if(a.getCapacitee() > 399) {
+					this.addAvion(a);
+					break;
+				}else System.out.println("Il n'y a pas d'avion adéquat disponible");
+				break;
+			}
+		}else if(dist > 1000) {
+			for(Avion a : D.getListAvion()) {
+				if(a.getCapacitee() > 149 && a.getCapacitee() < 399) {
+					this.addAvion(a);
+					break;
+				}else if(a.getCapacitee() > 149) {
+					this.addAvion(a);
+					break;
+				}else System.out.println("Il n'y a pas d'avion adéquat disponible");
+				break;
+			}
+		}else {
+			for(Avion a : D.getListAvion()) {
+				if(a.getCapacitee() < 149) {
+					this.addAvion(a);
+					break;
+				}else if(a.getCapacitee() > 149 && a.getCapacitee() < 399) {
+					this.addAvion(a);
+					break;
+				}else if(a.getCapacitee() > 399) {
+					this.addAvion(a);
+					break;
+				}else System.out.println("Il n'y a pas d'avion adéquat disponible");
+				break;
+			}
+		}
+		
+	}
  	
 }
